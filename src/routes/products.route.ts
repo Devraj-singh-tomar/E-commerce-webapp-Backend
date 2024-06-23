0e2b01f7006a588587cf7b12750de9472cfe189c
devraj singh tomar <devrajsingh.t11@gmail.com>
2024-06-23 21:38:31 +0530

    created get all admin products API

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.middleware.js";
 import {
+  getAdminProducts,
   getAllCategories,
   getLatestProducts,
   newProduct,
@@ -14,5 +15,7 @@ app.post("/new", adminOnly, singleUpload, newProduct);
 app.get("/latest", getLatestProducts);
 
 app.get("/categories", getAllCategories);
+
+app.get("/admin-products", getAdminProducts);
 
 export default app;
