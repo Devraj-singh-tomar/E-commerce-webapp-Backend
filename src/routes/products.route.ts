67d1ecc6f110c575e0f608c351fb197138c59ceb
devraj singh tomar <devrajsingh.t11@gmail.com>
2024-06-23 23:39:57 +0530

    created get , update and delete product API

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.middleware.js";
 import {
+  deleteProduct,
   getAdminProducts,
   getAllCategories,
   getLatestProducts,
+  getSingleProduct,
   newProduct,
+  updateProduct,
 } from "../controllers/product.controller.js";
 import { singleUpload } from "../middlewares/multer.js";
 
@@ -16,6 +19,13 @@ app.get("/latest", getLatestProducts);
 
 app.get("/categories", getAllCategories);
 
-app.get("/admin-products", getAdminProducts);
+app.get("/admin-products", adminOnly, getAdminProducts);
+
+// to get, update & delete product
+app
+  .route("/:id")
+  .get(getSingleProduct)
+  .put(adminOnly, singleUpload, updateProduct)
+  .delete(adminOnly, deleteProduct);
 
 export default app;
