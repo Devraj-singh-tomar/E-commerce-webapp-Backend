8e05fe302020c89dd7f4aa7fb70deee37c7ca4f4
devraj singh tomar <devrajsingh.t11@gmail.com>
2024-06-26 02:14:02 +0530

    created my orders and admin all orders API

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.middleware.js";
-import { newOrder } from "../controllers/order.controller.js";
+import {
+  allOrders,
+  myOrders,
+  newOrder,
+} from "../controllers/order.controller.js";
 
 const app = express.Router();
 
 // route- /api/v1/order/new
 app.post("/new", newOrder);
+
+// route- /api/v1/order/my
+app.get("/my", myOrders);
+
+// route- /api/v1/order/all
+app.get("/all", adminOnly, allOrders);
 
 export default app;
