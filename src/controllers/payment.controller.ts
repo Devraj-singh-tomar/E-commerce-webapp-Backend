ce6345c12c89c35a8a02bbdba16f649a10a868ed
devraj singh tomar <devrajsingh.t11@gmail.com>
2024-06-26 23:31:50 +0530

    created all coupon and delete coupon API

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -28,3 +28,25 @@ export const applyDiscount = TryCatch(async (req, res, next) => {
     discount: discount.amount,
   });
 });
+
+export const allCoupons = TryCatch(async (req, res, next) => {
+  const coupons = await Coupon.find({});
+
+  return res.status(200).json({
+    success: true,
+    coupons,
+  });
+});
+
+export const deleteCoupon = TryCatch(async (req, res, next) => {
+  const { id } = req.params;
+
+  const coupon = await Coupon.findByIdAndDelete(id);
+
+  if (!coupon) return next(new ErrorHandler("Invalid Coupon id", 400));
+
+  return res.status(200).json({
+    success: true,
+    message: `Coupon ${coupon.code} deleted successfully`,
+  });
+});
--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.middleware.js";
-import { applyDiscount, newCoupon } from "../controllers/payment.controller.js";
+import {
+  allCoupons,
+  applyDiscount,
+  deleteCoupon,
+  newCoupon,
+} from "../controllers/payment.controller.js";
 
 const app = express.Router();
 
-app.post("/coupon/new", newCoupon);
+app.post("/coupon/new", adminOnly, newCoupon);
 
 app.get("/discount", applyDiscount);
+
+app.get("/coupon/all", adminOnly, allCoupons);
+
+app.delete("/coupon/:id", adminOnly, deleteCoupon);
 
 export default app;
